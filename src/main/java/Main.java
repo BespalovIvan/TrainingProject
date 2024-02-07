103e0052f7a116736a2ff8a78edc187dd135f854
Ivan Bespalov <bespalovivan@mail.ru>
2024-02-07 22:36:41 +0700

    Попробовал написать метод поиска заказа по ID.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,10 @@
 import config.JDBCConnect;
 import config.Properties;
-import entity.Order;
 import repository.impl.OrderRepoImpl;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         OrderRepoImpl or = new OrderRepoImpl(new JDBCConnect(new Properties("C:\\Test\\JSONTest.json")));
-        List<Order> orderList = or.findAll();
-        for (Order o : orderList){
-            System.out.println(o);
-        }
+        System.out.println(or.findById(16L));
     }
 }
--- src/main/java/repository/impl/OrderRepoImpl.java
+++ src/main/java/repository/impl/OrderRepoImpl.java
@@ -6,10 +6,7 @@ import entity.Product;
 import entity.User;
 import repository.OrderRepo;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -56,7 +53,6 @@ public class OrderRepoImpl implements OrderRepo {
                     }
                 }
             }
-
             return orders;
         } catch (SQLException e) {
             throw new RuntimeException("invalid request");
@@ -65,7 +61,35 @@ public class OrderRepoImpl implements OrderRepo {
 
     @Override
     public Order findById(Long id) {
-        return null;
+        try {
+            Connection connection = jdbcConnect.createConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT o.id as order_id, " +
+                    "o.user_id,u.name,u.email,o.datetime as date, op.product_id , op.product_quantity," +
+                    " p.name as product_name, p.price " +
+                    "FROM orders as o JOIN order_products as op ON (o.id = op.order_id) " +
+                    "JOIN products as p ON (op.product_id = p.id) " +
+                    "JOIN users as u ON (o.user_id = u.id) WHERE order_id = ?");
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Product> productList = new ArrayList<>();
+            Order result = null;
+            while (resultSet.next()) {
+                if (productList.isEmpty()) {
+                    productList.add(new Product(resultSet.getLong(6),
+                            resultSet.getString(8), resultSet.getBigDecimal(9)));
+                    result = new Order(resultSet.getLong(1), new User(resultSet.getLong(2)
+                            , resultSet.getString(3), resultSet.getString(4)),
+                            productList, resultSet.getTimestamp(5));
+                } else {
+                    result.getProducts().add(new Product(resultSet.getLong(6),
+                            resultSet.getString(8), resultSet.getBigDecimal(9)));
+                }
+
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
